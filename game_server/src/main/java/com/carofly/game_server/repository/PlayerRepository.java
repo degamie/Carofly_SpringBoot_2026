42eaf23f482822027872cbc9f019adab63c9f90c
Degamie_sign <degamiesign@gmail.com>
2026-05-19 08:39:31 +0530

    PlayerSerialNum's Saving #1

--- game_server/src/main/java/com/carofly/game_server/repository/PlayerRepository.java
+++ game_server/src/main/java/com/carofly/game_server/repository/PlayerRepository.java
@@ -1,4 +1,4 @@
-//WID(16/05/2026)(Sarthak Mittal)(DegamieSign)#1.1,1.1,1s.1.1.1.1.1.1/1.1.1.1.1
+//WID(19/05/2026)(Sarthak Mittal)(DegamieSign)#1.1,1.1,1s.1.1.1.1.1.1/1.1.1.1.1.1
 package com.carofly.game_server.repository;
 
 //import com.carofly.game_server.entity.Player;
@@ -17,6 +17,7 @@ import java.util.List;
 
 @Repository
 public interface  PlayerRepository extends JpaRepository<String, Player> {
+    public List<Player>saveByPlayerSerialNum(Integer playerSerialNum);
     public List<Player>findByPlayerSerialNum(Integer playerSerialNum);
     public Pageable saveByPlayer(@RequestBody Player player,Pageable pageable);
     Page<Player> findByPlayers(String player, Pageable pageable);
