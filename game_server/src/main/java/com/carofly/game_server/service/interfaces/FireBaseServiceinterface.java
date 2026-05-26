74158c26921af0e15aef4f29edd3cd936a99d29c
Degamie_sign <degamiesign@gmail.com>
2026-05-26 13:06:57 +0530

    fetching playername by Firebasedb#1

--- game_server/src/main/java/com/carofly/game_server/service/interfaces/FireBaseServiceinterface.java
+++ game_server/src/main/java/com/carofly/game_server/service/interfaces/FireBaseServiceinterface.java
@@ -1,14 +1,15 @@
-////WID(21/05/2026)(Sarthak Mittal)(DegamieSign)#1
-//package com.carofly.game_server.service.interfaces;
-//
-//import jdk.jshell.spi.ExecutionControl;
-//
-//import java.util.concurrent.ExecutionException;
-//
-//public interface FireBaseServiceinterface  {
-//
-//    public String updateByPlayer(String playerId, String playerName, String playeremail);//updating Player in Server
-//    String getPlayers(String playerId) throws ExecutionControl.UserException, InterruptedException, ExecutionException;
-//
-//    String savePlayer(String playerId, String playerName, String playeremail) throws ExecutionException, InterruptedException;
-//}
+//WID(26/05/2026)(Sarthak Mittal)(DegamieSign)#1,1
+package com.carofly.game_server.service.interfaces;
+
+import jdk.jshell.spi.ExecutionControl;
+
+import java.util.concurrent.ExecutionException;
+
+public interface FireBaseServiceinterface  {
+    public String getplayername(String playername) throws  ExecutionControl.UserException,InterruptedException,ExecutionException;
+
+    public String updateByPlayer(String playerId, String playerName, String playeremail);//updating Player in Server
+    String getPlayers(String playerId) throws ExecutionControl.UserException, InterruptedException, ExecutionException;
+
+    String savePlayer(String playerId, String playerName, String playeremail) throws ExecutionException, InterruptedException;
+}
