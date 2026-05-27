93700a9ce09da40ec60de07d98efa0c3e83b7466
Degamie_sign <degamiesign@gmail.com>
2026-05-27 20:02:53 +0530

    OpenAIService_Impl#1

---
+++ game_server/src/main/java/com/carofly/game_server/service/OpenAIService.java
@@ -0,0 +1,10 @@
+//WID(27/5/2026)(Sarthak Mittal)(DegamieSign)#1
+package com.carofly.game_server.service;
+
+@Service
+public class OpenAIService {
+    public ChatClient chatClient;
+    public OpenAIService(ChatClient.Builder chatclient){
+
+    }
+}
