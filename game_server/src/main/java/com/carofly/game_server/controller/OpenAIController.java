85b00ac39e8fab4c1650564beb8c84b542dd1d65
Degamie_sign <degamiesign@gmail.com>
2026-05-28 21:25:39 +0530

    GenAI(NLP)'s Response #1

--- game_server/src/main/java/com/carofly/game_server/controller/OpenAIController.java
+++ game_server/src/main/java/com/carofly/game_server/controller/OpenAIController.java
@@ -1,10 +1,12 @@
-//WID(28/5/2026)(DegamieSign)
+//WID(28/5/2026)(DegamieSign)#1
 package com.carofly.game_server.controller;
 
 import com.carofly.game_server.service.OpenAIService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +17,10 @@ public class OpenAIController {
     public OpenAIService aiService;
     public OpenAIController(OpenAIService aiService){
         this.aiService=aiService;
+    }
+    @GetMapping("/chat")
+    public String   chat(@RequestParam(value = "message",defaultValue = "Which Car color you want to Select?,pLs tell")String message)
+    {
+        return aiService.generateresponse(message);
     }
 }
